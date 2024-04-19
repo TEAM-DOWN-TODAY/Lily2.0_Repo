b4666f916dddcae6a7d85b1d9069f2748eb817d4
TEAM-DOWN-TODAY <163845796+TEAM-DOWN-TODAY@users.noreply.github.com>
2024-04-19 21:14:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -45,8 +45,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/thecchub")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+af1GQdKbpEU2Y2Fl")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AXEL_POWERS")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Axel_ki_duniya")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False)) 
